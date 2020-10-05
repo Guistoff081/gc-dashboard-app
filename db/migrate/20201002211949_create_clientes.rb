6e8b7a66e2716f930c1d96c14694fcf0a34b7eae
Guistoff081 <guigolawliet13@gmail.com>
2020-10-05 11:48:53 -0300

    ajustando

---
+++ db/migrate/20201002211949_create_clientes.rb
@@ -0,0 +1,12 @@
+class CreateClientes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :clientes do |t|
+      t.string :nome
+      t.integer :cpf
+      t.string :telefone
+      t.text :endereco
+
+      t.timestamps
+    end
+  end
+end
