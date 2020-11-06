272863bdaa203ce0a672cc5066e97ac5ebb14205
Guistoff081 <guigolawliet13@gmail.com>
2020-11-06 17:04:16 -0300

    subindo alterações

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'pagina_dois/index'
   get 'admin/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
